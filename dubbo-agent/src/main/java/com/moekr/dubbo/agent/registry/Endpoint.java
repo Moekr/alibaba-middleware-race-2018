090a36a9378969c7b88648d37068d8b82ead284b
Moekr <highter@outlook.com>
2018-06-06 19:13:30 +0800

    增加连接数没用。。

--- dubbo-agent/src/main/java/com/moekr/dubbo/agent/registry/Endpoint.java
+++ dubbo-agent/src/main/java/com/moekr/dubbo/agent/registry/Endpoint.java
@@ -7,43 +7,30 @@ import com.moekr.dubbo.agent.protocol.codec.AgentMessageEncoder;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@EqualsAndHashCode(exclude = "channelThreadLocal")
-@ToString(exclude = "channelThreadLocal")
+@Data
 public class Endpoint {
-	private final ThreadLocal<Channel> channelThreadLocal = new ThreadLocal<>();
+	private final Channel channel;
 
 	private final String host;
 	private final int port;
 
-	@Getter
-	@Setter
 	private int weight;
 
 	public Endpoint(String host, int port, int weight) {
 		this.host = host;
 		this.port = port;
 		this.weight = weight;
-	}
 
-	public Channel getChannel() {
-		Channel channel = channelThreadLocal.get();
-		if (channel == null) {
-			channel = new NettyClientBootstrap(host, port, new ChannelInitializer<SocketChannel>() {
-				@Override
-				protected void initChannel(SocketChannel channel) {
-					channel.pipeline()
-							.addLast(new AgentMessageEncoder())
-							.addLast(new AgentMessageDecoder())
-							.addLast(new ConsumerResponseSender());
-				}
-			}).getSocketChannel();
-			channelThreadLocal.set(channel);
-		}
-		return channel;
+		this.channel = new NettyClientBootstrap(host, port, new ChannelInitializer<SocketChannel>() {
+			@Override
+			protected void initChannel(SocketChannel channel) {
+				channel.pipeline()
+						.addLast(new AgentMessageEncoder())
+						.addLast(new AgentMessageDecoder())
+						.addLast(new ConsumerResponseSender());
+			}
+		}).getSocketChannel();
 	}
 }
